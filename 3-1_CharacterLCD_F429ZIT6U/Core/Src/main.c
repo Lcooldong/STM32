c305e0030fbfc50a476e8e32827d4392eb2bcaa5
CoolDong <n_cooldong@naver.com>
2022-03-10 18:02:19 +0900

    .

--- 3-1_CharacterLCD_F429ZIT6U/Core/Src/main.c
+++ 3-1_CharacterLCD_F429ZIT6U/Core/Src/main.c
@@ -113,6 +113,10 @@ int main(void)
   //uint32_t a = 0;
   while (1)
   {
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	  HAL_Delay(500);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	  HAL_Delay(500);
 
     /* USER CODE END WHILE */
 
