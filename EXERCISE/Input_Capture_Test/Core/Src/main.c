a2156af9564f7bcdfa71484cf2a5aa8548602401
CoolDong <n_cooldong@naver.com>
2022-04-19 14:08:45 +0900

    .

---
+++ EXERCISE/Input_Capture_Test/Core/Src/main.c
@@ -0,0 +1,367 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "stdbool.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+volatile uint16_t capture[4];
+volatile uint16_t array[4];
+volatile uint16_t risingCP;
+volatile uint16_t fallingCP;
+uint8_t captureCNT = 0;
+uint32_t period, active, freq, duty;
+uint16_t width, DMAwidth = 0;
+uint16_t distance, DMAdistance = 0;
+bool ch1done = false;
+bool ch2done = false;
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+int _write(int file, char* p, int len)
+{
+	if(HAL_UART_Transmit(&huart3, (uint8_t *)p, len, 10) == HAL_OK) return len;
+	else return 0;
+}
+void DWT_Delay_us(volatile uint32_t microseconds);
+void TriggerEnable(void);
+uint32_t DWT_Delay_Init(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+  DWT_Delay_Init();
+
+  //HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_OC_Start_IT(&htim4, TIM_CHANNEL_1);		// Sonar
+  //htim3.State = HAL_TIM_STATE_READY;
+  HAL_TIM_IC_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t *)capture, 4);
+
+  HAL_TIM_Base_Start_IT(&htim3);
+  HAL_TIM_Base_Start_IT(&htim4);	// Sonar
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  printf("start this program \r\n");
+  HAL_Delay(1000);
+  while (1)
+  {
+	 //HAL_Delay(100);
+	 printf("%lx %x [%d] %5d | %5d | %5d | %5d | width : %5d\r\n",TIM3->CCER & TIM_CCER_CC1P, 0x01 << 1 , captureCNT, capture[0], capture[1], capture[2], capture[3], DMAwidth);
+	 //printf("%lx %x [%d] %5d | %5d | %5d | %5d | width : %5d\r\n",TIM3->CCER & TIM_CCER_CC1P, 0x01 << 1 , captureCNT, array[0], array[1], array[2], array[3], DMAwidth);
+	 //printf("%lx %x %5d-%5d DMAWidth : %5d => DMAdistance : %4d cm | period : %5ld | freq : %3ld | duty : %3ld\r\n",TIM3->CCER & TIM_CCER_CC1P, 0x01 << 1 ,capture[0], capture[1], DMAwidth, DMAdistance, period ,freq, duty);
+
+
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 180;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Activate the Over-Drive mode
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+/* DMA */
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if(htim->Instance == TIM4)
+	{
+		GPIOB->ODR |= 0x01 << 6;  // PB 6 SET
+		GPIOB->ODR |= LD1_Pin;
+		//TriggerEnable();
+	}
+
+}
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	if(htim->Instance == TIM4)	// Pulse 7
+	{
+		GPIOB->ODR &= ~(0x01 << 6);	// PB 6	RESET, Pulse
+		GPIOB->ODR &= ~LD1_Pin;
+	}
+}
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+
+	if(htim->Instance == TIM3 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+	{
+
+
+		/*   */
+		if((TIM3->CCER & TIM_CCER_CC1P) != TIM_CCER_CC1P)
+		{
+			array[captureCNT] = capture[captureCNT];	// f(past <- get) - r(current <- capture[1, 3]) - f(future)
+			TIM3->CCER |= TIM_CCER_CC1P;		// Rising -> Falling (0 -> 1)
+			GPIOB->ODR |= LD2_Pin;
+			GPIOB->ODR &= ~LD3_Pin;
+			ch1done = true;
+		}
+		else
+		{
+			array[captureCNT] = capture[captureCNT];		// r(past <- get) - f(current <- capture[0, 2]) - r(future)
+			TIM3->CCER &= ~TIM_CCER_CC1P;		// Falling -> Rising	(1 -> 0)
+			GPIOB->ODR |= LD3_Pin;
+			GPIOB->ODR &= ~LD2_Pin;
+			ch1done = false;
+
+		}
+
+
+		if(!ch1done)
+		{
+					if(array[0] > array[1]){
+						DMAwidth = array[0] - array[1];
+					}
+					else
+					{
+						DMAwidth = TIM3->ARR - array[1] + array[0];
+					}
+		//
+		//			 DMAdistance = DMAwidth/58;
+		//			 if(DMAdistance > 400) DMAdistance = 400;
+		//
+		//			 if(capture[0] > capture[2])
+		//			 {
+		//				 period = TIM3->ARR - capture[0] + capture[2];
+		//			 }
+		//			 else
+		//			 {
+		//				 period = capture[2] - capture[0];
+		//			 }
+		//
+		//			 period += TIM3->ARR;
+		//			 freq = (HAL_RCC_GetPCLK1Freq() * 2)/(TIM3->PSC + 1);
+		//			 freq = freq / period;
+		//
+		//			 duty = DMAwidth * 100/period;
+		}
+
+		if(captureCNT > 3) captureCNT = 0;
+		else captureCNT++;
+
+	}
+}
+
+uint32_t DWT_Delay_Init(void)
+{
+  /* Disable TRC */
+  CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk; // ~0x01000000;
+  /* Enable TRC */
+  CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk; // 0x01000000;
+
+  /* Disable clock cycle counter */
+  DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk; //~0x00000001;
+  /* Enable  clock cycle counter */
+  DWT->CTRL |=  DWT_CTRL_CYCCNTENA_Msk; //0x00000001;
+
+  /* Reset the clock cycle counter value */
+  DWT->CYCCNT = 0;
+
+     /* 3 NO OPERATION instructions */
+     __ASM volatile ("NOP");
+     __ASM volatile ("NOP");
+  __ASM volatile ("NOP");
+
+  /* Check if clock cycle counter has started */
+     if(DWT->CYCCNT)
+     {
+       return 0; /*clock cycle counter started*/
+     }
+     else
+  {
+    return 1; /*clock cycle counter not started*/
+  }
+}
+
+void DWT_Delay_us(volatile uint32_t microseconds)
+{
+  uint32_t clk_cycle_start = DWT->CYCCNT;
+
+  /* Go to number of cycles for system */
+  microseconds *= (HAL_RCC_GetHCLKFreq() / 1000000);
+
+  /* Delay till end */
+  while ((DWT->CYCCNT - clk_cycle_start) < microseconds);
+}
+
+void TriggerEnable(void)
+{
+	GPIOB->ODR &= ~TRIGGER_Pin;	// initialize
+	DWT_Delay_us(2);
+	GPIOB->ODR |= TRIGGER_Pin;	// Trigger signal
+	DWT_Delay_us(11);
+	GPIOB->ODR &= ~TRIGGER_Pin;	// shut down
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
